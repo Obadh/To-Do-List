9ef85b61cecb31480768b13114e97ebee8f955ec
Obadh Koujan <obadhk@live.co.uk>
2016-07-30 23:26:07 +0200

    First Commit

---
+++ js/controllers/todoController.js
@@ -0,0 +1,14 @@
+function todoController($scope) {
+	$scope.myInput = "";
+    $scope.toDoList = [];
+
+    $scope.addItem = function() {
+    	// if($scope.myInput) {
+    		$scope.toDoList.push($scope.myInput);
+    		$scope.myInput = "";
+    	// }
+    }
+		$scope.deleteItem = function () {
+			$scope.toDoList.pop();
+		}
+}
